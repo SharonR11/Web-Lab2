c809923c771de7ec589bb93272f33ef32f8357ff
SharonR11 <sharon.rudas@tecsup.edu.pe>
2023-09-07 17:39:38 -0500

    commit 3

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require ('express');
 const app = express();
+//indicando que se cargen archivos static
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => { 
     res.sendFile('./static/inicio.html',{
@@ -11,8 +13,8 @@ app.get('/form', (req, res) => {
         root:__dirname
     })
 })
-app.get('/lista', (req, res) => { 
-    res.sendFile('./static/lista.html',{
+app.get('/update', (req, res) => { 
+    res.sendFile('./static/actualizar.html',{
         root:__dirname
     })
 })
@@ -23,3 +25,20 @@ console.log(`server on port ${3000}`)
 app.use((req,res)=>{
     res.status(404).send('No se encontro tu pagina.....!!!')
 })
+const str = '[{ "name": "Jose Perez", "edad": 42 },' +
+           '{ "name": "Carlos Díaz", "edad": 24 },' +
+           '{ "name": "Peter Mamani", "edad": 35 }]';
+
+const obj = JSON.parse(str);
+
+app.all('/user', (req, res, next) => { 
+    console.log('Por Aqui Paso');
+    next();
+});
+//como se recorre un objeto json y como obtener los datos
+app.get('/', (req, res) => { 
+    for (var i = 0; i < obj.length; i++) {
+        console.log('Nombre: ' + obj[i].name + ', Edad:' + obj[i].edad);
+      }
+	res.send('Parseo JSON correcto');
+});
